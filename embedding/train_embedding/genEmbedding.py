3fa80b3db36edfd28eff5f8977135c27dbd47e8c
raywu0123 <b05901189@ntu.edu.tw>
2019-11-22 15:36:52 +0800

    fix code style

--- embedding/train_embedding/genEmbedding.py
+++ embedding/train_embedding/genEmbedding.py
@@ -3,7 +3,8 @@ import os
 
 dir_name = "data/"
 
-def getFilenames(start_percent, end_percent):
+
+def get_file_names(start_percent, end_percent):
     subprocess.call(["wget", "https://dumps.wikimedia.org/enwiki/latest/", "-O", dir_name + "index.html"])
     target_string = "<a href=\"enwiki-latest-pages-articles"
     target_len = len(target_string)
@@ -17,23 +18,20 @@ def getFilenames(start_percent, end_percent):
                 filenames.append(line[start_idx:end_idx])
 
         num_files = len(filenames)
-        return filenames[ int(num_files*start_percent) : int(num_files*end_percent) ]
+        return filenames[int(num_files*start_percent): int(num_files*end_percent)]
 
-def Downlad_and_Parse(filenames, start_percent, end_percent):
+
+def download_and_parse(filenames, start_percent, end_percent):
     output_f = dir_name + str(start_percent) + "to" + str(end_percent) + ".txt"
     if os.path.exists(output_f):
         print("WARN:", output_f, "has already existed")
         return
     output_f = open(output_f, "w")
-    for i,f in enumerate(filenames):
+    for i, f in enumerate(filenames):
         script = "wikifil.pl"
         url = "https://dumps.wikimedia.org/enwiki/latest/" + f
         f = dir_name + f
         extracted_f = f[:-4]
-        if i == 0:
-            redirect = ">"
-        else:
-            redirect= ">>"
         if not os.path.exists(f):
             print("wget", url, "-O", f)
             subprocess.call(["wget", url, "-O", f])
@@ -52,6 +50,8 @@ def Downlad_and_Parse(filenames, start_percent, end_percent):
         subprocess.call(["rm", extracted_f])
     output_f.close()
 
-(start_percent, end_percent) = (0, 0.1)
-fnames = getFilenames(start_percent, end_percent)
-Downlad_and_Parse(fnames, start_percent, end_percent)
+
+if __name__ == '__main__':
+    (start_percent, end_percent) = (0, 0.1)
+    fnames = get_file_names(start_percent, end_percent)
+    download_and_parse(fnames, start_percent, end_percent)
